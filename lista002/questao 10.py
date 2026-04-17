57addb7f2917266a686ab9335e804591dc8ed33c
Gabriel V. <166523073+ProgrammerGV@users.noreply.github.com>
2026-04-17 22:35:39 +0000

    feito

---
+++ lista002/questao 10.py
@@ -0,0 +1,14 @@
+frase = "Python é muito legal"
+
+palavra1 = frase[0:6]  
+palavra2 = frase[7:8]  
+palavra3 = frase[9:14]  
+palavra4 = frase[15:20] 
+
+print(palavra1)
+print(palavra2)
+print(palavra3)
+print(palavra4)
+
+print (f"Tamanho da frase: {len(frase)}")
+print (f"tamanho da primeira palavra: {len(palavra1)}\nsegunda: {len(palavra2)}\nterceira: {len(palavra3)}\nquarta: {len(palavra4)}")
---
+++ lista002/questao 9.py
@@ -0,0 +1,5 @@
+frase = input("Digite a frase: ")
+
+frase_invertida = frase[::-1]
+
+print(frase_invertida)
